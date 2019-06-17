2a6ab213de3590448c515307e5781a1ebfeffb6e
strinsberg <stevendkom@gamil.com>
2019-06-16 21:21:20 -0600

    Added failing test implementaion to MoveEntity tests.

--- tests/test_event_single.py
+++ tests/test_event_single.py
@@ -1,15 +1,37 @@
 import unittest
+import dgsl_engine.entity_containers as container
+import dgsl_engine.event_single as single
+import dgsl_engine.entity_base as entity
+
+ID = "1234"
+ID2 = "5678"
+ID3 = "9876"
+ID4 = "5432"
+ID5 = "entity"
 
 
 class TestMoveEntity(unittest.TestCase):
     def setUp(self):
-        pass
+        self.entity = entity.Entity(ID5)
+        self.container = container.Container(ID)
+        self.room = container.Room(ID2)
+        self.player = container.Player(ID3)
+        self.move = single.MoveEntity(ID4)
 
     def test_init(self):
-        pass
+        self.move.id = ID4
 
     def test_execute(self):
-        pass
+        self.container.add(self.entity)
+        self.move.destination = self.room
+        self.move.execute(self.entity)
+        self.assertTrue(self.room.inventory.has_item(ID5))
+        self.assertFalse(self.container.inventory.has_item(ID5))
 
-    def test_execute_throws(self):
-        pass
+    def test_execute_raises(self):
+        self.room.add(self.player)
+        self.move.destination = self.container
+        with self.assertRaises(container.ContainerError):
+            self.move.execute(self.player)
+        self.assertTrue(self.room.inventory.has_item(ID3))
+        self.assertFalse(self.container.inventory.has_item(ID3))
