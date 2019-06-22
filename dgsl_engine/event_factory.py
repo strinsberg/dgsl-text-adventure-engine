e398cb06e88cf45438292171400b796d4cb5abc8
strinsberg <stevendkom@gamil.com>
2019-06-22 15:39:14 -0600

    Added tests and implementation for starting event factory. All tests pass and 100% coverage on event factory.

--- dgsl_engine/event_factory.py
+++ dgsl_engine/event_factory.py
@@ -1,6 +1,42 @@
+from . import event_base
+from . import event_single
+from . import event_single_decorators as evs
+from . import exceptions
+
+
 class EventFactory:
     def new(self, obj):
-        return {}
+        try:
+            type_ = obj['type']
+            id_ = obj['id']
+            if type_ in ['event',
+                         'inform']:  # remove inform when editor is fixed
+                event = event_base.Event(id_)
+            elif type_ == 'move':
+                event = event_single.MoveEntity(id_)
+            else:
+                raise exceptions.InvalidParameterError(
+                    "Error: invalid obj of type " + type_)
+
+            return self._setup(event, obj)
+
+        except KeyError as err:
+            raise exceptions.InvalidParameterError(
+                "Error: object is not complete: " + str(err))
 
     def _setup(self, event, obj):
-        pass
+        event.only_once = num_to_bool(obj['once'])
+        return self._add_decorators(event, obj)
+
+    def _add_decorators(self, event, obj):
+        # Proabably split this into 2 methods or multiple private functions
+        # to accomadate single and multiple decorators once they exist
+        if 'message' in obj:
+            decorated = evs.MessageDecorator(event, obj['message'])
+        else:
+            return event
+        return decorated
+
+
+def num_to_bool(num):
+    return num == 1
--- tests/test_event_factory.py
+++ tests/test_event_factory.py
@@ -1,9 +1,42 @@
 import unittest
 import dgsl_engine.event_factory as factory
+import dgsl_engine.exceptions as exceptions
+
+OBJ = {'id': '1234', 'once': 1}
 
 
 class TestEventFactory(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.fact = factory.EventFactory()
+        self.obj = OBJ
+
+    def test_new_entity(self):
+        self.obj['type'] = 'event'
+        event = self.fact.new(self.obj)
+        self.assertEqual(event.id, OBJ['id'])
+        self.assertFalse(event.is_done)
+        self.assertTrue(event.only_once)
+
+    def test_new_move(self):
+        self.obj['type'] = 'move'
+        event = self.fact.new(self.obj)
+        self.assertEqual(event.id, OBJ['id'])
+
+    def test_new_with_message_decorator(self):
+        self.obj['type'] = 'event'
+        self.obj['message'] = 'Some real interesting message'
+        event = self.fact.new(self.obj)
+        self.assertEqual(event.id, OBJ['id'])
+        self.assertEqual(event.execute(None), self.obj['message'])
+
+    def test_new_unfinished_object_throws(self):
+        with self.assertRaises(exceptions.InvalidParameterError):
+            self.fact.new({'id': '09r8320'})
+
+    def test_new_invalid_type(self):
+        with self.assertRaises(exceptions.InvalidParameterError):
+            self.obj['type'] = 'not_real'
+            self.fact.new(self.obj)
 
 
 # Main #################################################################
