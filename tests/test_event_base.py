3c44b1800725aca0f40964e15eb9a0100d1c9dba
strinsberg <stevendkom@gamil.com>
2019-06-22 11:56:00 -0600

    Added passing tests to event_base for current basic implementation. 100% coverage.

--- tests/test_event_base.py
+++ tests/test_event_base.py
@@ -1,9 +1,38 @@
 import unittest
 import dgsl_engine.event_base as event_base
+import dgsl_engine.entity_base as entity_base
+
+ID = '1234'
+RESULT = ''
+
+# Mocks ################################################################
+
+
+class MockVisitor:
+    def visit_event(self, event):
+        self.result = event.id
+
+
+# Tests ################################################################
 
 
 class TestEventBase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.event = event_base.Event(ID)
+        self.entity = entity_base.Entity(ID)
+
+    def test_init(self):
+        self.assertFalse(self.event.is_done)
+        self.assertFalse(self.event.only_once)
+
+    def test_execute(self):
+        # Will need to change when event gets subjects
+        self.assertEqual(self.event.execute(self.entity), RESULT)
+
+    def test_visit(self):
+        visitor = MockVisitor()
+        self.event.accept(visitor)
+        self.assertEqual(visitor.result, ID)
 
 
 # Main #################################################################
