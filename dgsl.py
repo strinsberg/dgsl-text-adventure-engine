7dbcad1e302d1135d2333026896d160c4b86bb9c
strinsberg <stevendkom@gamil.com>
2019-06-23 14:31:09 -0600

    Created a game_factory module and moved the code from dgsl.py that set everything up into it. All dgsl.py does now is ask for a world name and create a game and run it.

--- dgsl.py
+++ dgsl.py
@@ -1,28 +1,5 @@
 if __name__ == '__main__':
-    import os
-    import json
-    import dgsl_engine.game as game
-    import dgsl_engine.user_input as user_input
-    import dgsl_engine.world as world
-    import dgsl_engine.actions as actions
-    import dgsl_engine.visitors as visitors
-
-    parser = user_input.Parser()
-
-    collector_factory = visitors.EntityCollectorFactory()
-    menu_factory = user_input.MenuFactory()
-    action_factory = actions.ActionFactory()
-    resolver = actions.ActionResolver(collector_factory, menu_factory,
-                                      action_factory)
-
-    world_path = "testing_ground.world"
-    home = os.path.expanduser('~')
-    path = os.path.join(home, ".dgsl/worlds", world_path)
-
-    with open(path) as file:
-        world_json = json.load(file)
-
-    game_world = world.WorldFactory().new(world_json)
-
-    new_game = game.Game(game_world, parser, resolver)
-    new_game.run()
+    from dgsl_engine.game_factory import GameFactory
+    world_name = 'testing ground'
+    game = GameFactory().new(world_name)
+    game.run()
---
+++ dgsl_engine/game_factory.py
@@ -0,0 +1,33 @@
+import os
+import json
+import dgsl_engine.game as game
+import dgsl_engine.user_input as user_input
+import dgsl_engine.world as world
+import dgsl_engine.actions as actions
+import dgsl_engine.visitors as visitors
+
+
+class GameFactory:
+    def new(self, world_name):
+        parser = user_input.Parser()
+
+        collector_factory = visitors.EntityCollectorFactory()
+        menu_factory = user_input.MenuFactory()
+        action_factory = actions.ActionFactory()
+        resolver = actions.ActionResolver(collector_factory, menu_factory,
+                                          action_factory)
+
+        world_path = self._name_to_path(world_name)
+        home = os.path.expanduser('~')
+        file_path = os.path.join(home, ".dgsl/worlds", world_path)
+
+        with open(file_path) as file:
+            world_json = json.load(file)
+
+        game_world = world.WorldFactory().new(world_json)
+
+        return game.Game(game_world, parser, resolver)
+
+    def _name_to_path(self, name):
+        words = name.split()
+        return "_".join(words) + ".world"
