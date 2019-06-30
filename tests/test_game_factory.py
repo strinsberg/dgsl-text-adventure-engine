4d3b35e8ed273bd8b51a4926084de706f278af45
strinsberg <stevendkom@gamil.com>
2019-06-30 13:39:45 -0600

    Added a game factory test. I thought I had but suddenly coverage on it was gone. So must have missed it or deleted it. All tests passing and 100% coverage.

---
+++ tests/test_game_factory.py
@@ -0,0 +1,22 @@
+import unittest
+import dgsl_engine.game_factory as factory
+
+
+# Tests ################################################################
+
+class TestGameFactory(unittest.TestCase):
+    def setUp(self):
+        fact = factory.GameFactory()
+        self.game = fact.new('testing_ground')
+
+    def test_new(self):
+        # Not really an adequate tests, but it runs the code.
+        self.assertEqual(self.game.world.name, 'testing ground')
+        self.assertEqual(self.game.world.version, '0.0')
+        self.assertEqual(self.game.world.welcome, 'fun is waiting!')
+
+
+# Main #################################################################
+
+if __name__ == '__main__':
+    unittest.main()
