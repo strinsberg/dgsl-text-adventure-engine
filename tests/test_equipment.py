2be7d13d1deb9ef98f7e72f3934a081b7c91b85b
strinsberg <stevendkom@gmail.com>
2019-07-19 21:52:27 -0600

    Updated equipment tests. All tests passing and 100% coverage on module.

--- tests/test_equipment.py
+++ tests/test_equipment.py
@@ -1,20 +1,41 @@
 import unittest
+import unittest.mock as mock
 import dgsl_engine.equipment as equipment
-import dgsl_engine.entity_factory as ent_fact
-from . import json_objects
 
+# Constants ############################################################
+
+ID = "03ru02r0"
+SLOT = 'head'
+PROTECTS = ['cold', 'wind']
+MUST = 0
+
+
+# Tests ################################################################
 
 class TestEquipment(unittest.TestCase):
     def setUp(self):
-        self.equipment = ent_fact.EntityFactory().new(json_objects.EQUIPMENT)
+        self.equipment = equipment.Equipment(ID)
+        self.equipment.slot = SLOT
+        self.equipment.protects = PROTECTS
+        self.equipment.must_equip = MUST
 
     def test_init(self):
-        pass
+        self.assertEqual(self.equipment.slot, SLOT)
+        self.assertEqual(self.equipment.protects, PROTECTS)
+        self.assertEqual(self.equipment.must_equip, MUST)
 
     def test_accept(self):
-        pass
+        visitor = mock.MagicMock()
+        self.equipment.accept(visitor)
+        visitor.visit_equipment.assert_called_with(self.equipment)
 
     def test_repr(self):
         rep = "<Equipment: '{}', Name: '{}'>".format(
             self.equipment.spec.id, self.equipment.spec.name)
         self.assertEqual(repr(self.equipment), rep)
+
+
+# Main #################################################################
+
+if __name__ == '__main__':
+    unittest.main()
